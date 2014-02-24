ed8737179abc2e9ab7fc782fbb8d9c2434ef8045
adamzy <rightcoset@gmail.com>
2014-02-24 23:02:56 +0800

    add my.el

---
+++ my.el
@@ -0,0 +1,23 @@
+;; the main control file
+
+;; el-get first
+(require 'my-elget)
+
+;; ui and misc init
+(require 'my-ui)
+(require 'my-misc)
+
+;; then plugins
+(require 'my-complete)
+(require 'my-auctex)
+(require 'my-pairedit)
+(require 'my-smex)
+(require 'my-spell)
+
+;; now modes
+(require 'my-org)
+(require 'my-lisp)
+(require 'my-go)
+
+;; Done
+(provide 'my)
