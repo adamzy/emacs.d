5ce8d0836e0d24e6970d100ebe3f5765363ab331
adamzy <rightcoset@gmail.com>
2014-02-25 20:35:50 +0800

    add byte-compile-buffer when save a file

--- my-misc.el
+++ my-misc.el
@@ -30,5 +30,15 @@
             (when server-buffer-clients
               (local-set-key (kbd "C-x C-c") 'server-edit))))
 
+;; byte-compile-buffer when save file
+(defun byte-compile-current-buffer ()
+  "`byte-compile' current buffer if it's emacs-lisp-mode and compiled file exists."
+  (interactive)
+  (when (and (eq major-mode 'emacs-lisp-mode)
+             (file-exists-p (byte-compile-dest-file buffer-file-name)))
+    (byte-compile-file buffer-file-name)))
+
+(add-hook 'after-save-hook 'byte-compile-current-buffer)
+
 
 (provide 'my-misc)
