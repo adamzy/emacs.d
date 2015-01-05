d91424a74ca362383721791b0a7e909b19644bff
adamzy <rightcoset@gmail.com>
2015-01-05 19:39:16 +0800

    move my*.el to my-config to satisfy emacs24

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-(add-to-list 'load-path (expand-file-name "~/.emacs.d"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/my-config"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/my-others"))
 
 (require 'my)
